b2372c304bd64062d83d858c28cfcddd1e4a4673
Frella-dev <analyst@frellainternational.com>
2026-06-22 17:26:58 +0530

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,127 +1,108 @@
-from priority import (
-    prepare_customers,
-    get_pending_customers
-)
+from priority import (prepare_customers,get_pending_customers)
 
-from route_engine import (
-    build_master_route,
-    split_route_by_distance,
-    route_summary
-)
+from route_engine import (create_zones,order_zones,build_master_route,split_route_by_distance,route_summary)
 
+OFFICE_LAT = 6.827305661191226OFFICE_LON = 79.95698907652856
 
-OFFICE_LAT = 6.827305661191226
-OFFICE_LON = 79.95698907652856
+def build_nationwide_plan(df,daily_limit=160):
 
-
-def build_area_plan(
+df = create_zones(
     df,
-    area,
-    daily_limit
-):
+    n_zones=6
+)
 
-    area_df = df[
-        df["Town"]
-        .astype(str)
-        .str.upper()
-        ==
-        str(area).upper()
+zone_order = order_zones(
+    df,
+    OFFICE_LAT,
+    OFFICE_LON
+)
+
+all_days = []
+
+current_lat = OFFICE_LAT
+current_lon = OFFICE_LON
+
+for zone in zone_order:
+
+    zone_df = df[
+        df["Zone"] == zone
     ].copy()
 
-    if len(area_df) == 0:
-
-        return []
-
     route = build_master_route(
-        area_df,
-        OFFICE_LAT,
-        OFFICE_LON
+        zone_df,
+        current_lat,
+        current_lon
     )
 
-    return split_route_by_distance(
+    zone_days = split_route_by_distance(
         route,
+        daily_limit,
+        max_stops=10
+    )
+
+    all_days.extend(
+        zone_days
+    )
+
+    if len(route) > 0:
+
+        current_lat = route[-1][
+            "Latitude"
+        ]
+
+        current_lon = route[-1][
+            "Longitude"
+        ]
+
+return all_days
+
+def build_area_plan(df,area,daily_limit=160):
+
+area_df = df[
+    df["Town"]
+    .astype(str)
+    .str.upper()
+    ==
+    str(area).upper()
+].copy()
+
+route = build_master_route(
+    area_df,
+    OFFICE_LAT,
+    OFFICE_LON
+)
+
+return split_route_by_distance(
+    route,
+    daily_limit,
+    max_stops=10
+)
+
+def create_plan(df,ors_api_key=None,mode="nationwide",area=None,daily_limit=160):
+
+df = prepare_customers(
+    df
+)
+
+df = get_pending_customers(
+    df
+)
+
+if mode == "area":
+
+    return build_area_plan(
+        df,
+        area,
         daily_limit
     )
 
-
-def build_nationwide_plan(
+return build_nationwide_plan(
     df,
     daily_limit
-):
-    """
-    One continuous Sri Lanka route.
-    """
+)
 
-    route = build_master_route(
-        df,
-        OFFICE_LAT,
-        OFFICE_LON
-    )
+def get_plan_summary(days):
 
-    return split_route_by_distance(
-        route,
-        daily_limit
-    )
-
-
-def create_plan(
-    df,
-    ors_api_key=None,
-    mode="nationwide",
-    area=None,
-    daily_limit=160
-):
-
-    df = prepare_customers(
-        df
-    )
-
-    df = get_pending_customers(
-        df
-    )
-
-    if len(df) == 0:
-
-        return []
-
-    if mode == "area":
-
-        days = build_area_plan(
-            df,
-            area,
-            daily_limit
-        )
-
-    else:
-
-        days = build_nationwide_plan(
-            df,
-            daily_limit
-        )
-
-    return days
-
-
-def get_plan_summary(
+return route_summary(
     days
-):
-
-    return route_summary(
-        days
-    )
-
-
-def get_day_stops(
-    days,
-    day_no
-):
-
-    if day_no < 1:
-        return []
-
-    if day_no > len(days):
-        return []
-
-    return days[
-        day_no - 1
-    ]
+)
